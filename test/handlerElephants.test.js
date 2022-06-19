0268b9e9252cb8acccff0ee40f0d3c47240686e0
Fernando J Monteiro <ferjmonteiro@gmail.com>
2022-06-19 15:43:20 -0300

    requisito 6 e 7

--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -1,3 +1,56 @@
 const handlerElephants = require('../src/handlerElephants');
 
-describe('Testes da função HandlerElephants', () => {});
+describe('Testes da função HandlerElephants', () => {
+  it('retorna a quantidade de elefantes', () => {
+    const actual = handlerElephants('count');
+    const expected = 4;
+    expect(actual).toBe(expected);
+  });
+
+  it('retorna um array com a relação dos nomes de todos os elefantes', () => {
+    const actual = handlerElephants('names');
+    const expected = ['Ilana', 'Orval', 'Bea', 'Jefferson'];
+    expect(actual).toEqual(expected);
+    expect(actual).toContain('Jefferson');
+  });
+
+  it('retorna a média de idade dos elefantes', () => {
+    const actual = handlerElephants('averageAge');
+    const expected = 10.5;
+    expect(actual).toBe(expected);
+  });
+
+  it('retorna a localização dos elefantes dentro do Zoológico', () => {
+    const actual = handlerElephants('location');
+    const expected = 'NW';
+    expect(actual).toBe(expected);
+  });
+
+  it('retorna a popularidade dos elefantes', () => {
+    const actual = handlerElephants('popularity');
+    const expected = 5;
+    expect(actual).toBe(expected);
+  });
+
+  it('retorna um array com a relação de dias em que é possível visitar os elefantes', () => {
+    const actual = handlerElephants('availability');
+    const expected = ['Friday', 'Saturday', 'Sunday', 'Tuesday'];
+    expect(actual).toEqual(expected);
+  });
+
+  it('verifica se a função é case sensitive', () => {
+    const actual = handlerElephants('Availability');
+    const expected = null;
+    expect(actual).toBe(expected);
+  });
+
+  it('retorna null ao receber um parâmetro inválido', () => {
+    const actual = handlerElephants('');
+    const expected = null;
+    expect(actual).toBe(expected);
+  });
+  it('não passando argumentos retorna undefined', () => {
+    const actual = handlerElephants();
+    expect(actual).toBe(undefined);
+  });
+});
