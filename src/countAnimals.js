c4ba315a55ef32bc7de3eafc99bc99da3daabb35
Fernando J Monteiro <ferjmonteiro@gmail.com>
2022-06-19 02:04:42 -0300

    requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,19 @@
 const data = require('../data/zoo_data');
 
-function countAnimals(animal) {
-  // seu código aqui
-}
+const { species } = data;
 
+function countAnimals(animal) {
+  if (animal === undefined) {
+    const aniObj = {};
+    species.forEach((item) => {
+      aniObj[item.name] = item.residents.length;
+    });
+    return aniObj;
+  }
+  const { residents } = species.find((specie) => specie.name === animal.specie);
+  if (animal.sex) {
+    return residents.filter((resident) => resident.sex === animal.sex).length;
+  }
+  return residents.length;
+}
 module.exports = countAnimals;
