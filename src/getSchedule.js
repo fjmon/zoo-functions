bf10ff4e464c42b314b97a4fd836c23ccf51e654
Fernando J Monteiro <ferjmonteiro@gmail.com>
2022-06-20 14:26:58 -0300

    requisito 9

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,41 @@
 const data = require('../data/zoo_data');
 
-function getSchedule(scheduleTarget) {
-  // seu código aqui
+const { species, hours } = data;
+
+function diaSemana(dia) {
+  const { open, close } = hours[dia];
+  const animal = species.filter(({ availability }) => availability
+    .includes(dia)).map(({ name }) => name);
+  if (dia === 'Monday') {
+    return {
+      officeHour: 'CLOSED',
+      exhibition: 'The zoo will be closed!',
+    };
+  }
+  if (dia !== 'Monday') {
+    return {
+      officeHour: `Open from ${open}am until ${close}pm`,
+      exhibition: animal,
+    };
+  }
 }
+const funcionamento = (semana) => semana.reduce((accumulator, day) => {
+  accumulator[day] = diaSemana(day);
+  return accumulator;
+}, {});
+
+function getSchedule(scheduleTarget) {
+  const animais = species.map(({ name }) => name);
+  const dias = Object.keys(hours);
+  if (animais.includes(scheduleTarget)) {
+    return species.find(({ name }) => name === scheduleTarget).availability;
+  }
+  if (dias.includes(scheduleTarget)) {
+    return { [scheduleTarget]: diaSemana(scheduleTarget) };
+  }
+  return funcionamento(dias);
+}
+
+console.log(getSchedule('Monday'));
 
 module.exports = getSchedule;
