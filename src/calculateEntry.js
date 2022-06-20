e15dcf402c4893f47b1d5156761df6456b2684b3
Fernando J Monteiro <ferjmonteiro@gmail.com>
2022-06-20 00:21:59 -0300

    requisito 8

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,12 +1,21 @@
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
-  
+  const contador = {};
+  const child = entrants.filter((item) => item.age < 18).length;
+  const adult = entrants.filter((item) => item.age >= 18 && item.age < 50).length;
+  const senior = entrants.filter((item) => item.age >= 50).length;
+  contador.child = child;
+  contador.adult = adult;
+  contador.senior = senior;
+  return contador;
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (!entrants || Object.keys(entrants).length === 0) return 0;
+  const { child, adult, senior } = countEntrants(entrants);
+  const { child: entChild, adult: entAdult, senior: entSenior } = data.prices;
+  return (child * entChild + adult * entAdult + senior * entSenior);
 }
 
 module.exports = { calculateEntry, countEntrants };
