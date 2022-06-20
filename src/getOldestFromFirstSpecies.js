5375238a96acacec9c21fa0e6cba7b1784f826db
Fernando J Monteiro <ferjmonteiro@gmail.com>
2022-06-20 15:52:06 -0300

    requisito 10

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,15 @@
 const data = require('../data/zoo_data');
 
+const { employees, species } = data;
+
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const animais = employees.find((item) => id.includes(item.id)).responsibleFor[0];
+  return Object.values(species.find((anim) => anim.id === animais).residents.reduce((acc, curr) => {
+    if (acc.age > curr.age) {
+      return acc;
+    }
+    return curr;
+  }));
 }
 
 module.exports = getOldestFromFirstSpecies;
