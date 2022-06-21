7fa8fcde1a704e6cacf6be59e3dce74e1b716bb7
Fernando J Monteiro <ferjmonteiro@gmail.com>
2022-06-20 21:24:44 -0300

    requisito 11

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,28 @@
 const data = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
+const { employees, species } = data;
+let buscaPeao;
+const filtraPeao = employees.reduce((acc, curr) => {
+  const obj = {
+    id: curr.id,
+    fullName: `${curr.firstName} ${curr.lastName}`,
+    species: curr.responsibleFor.map((id) => species.find((specie) => specie.id === id).name),
+    locations: curr.responsibleFor.map((id) => species.find((specie) => specie.id === id).location),
+  };
+  acc.push(obj);
+  return acc;
+}, []);
+
+function getEmployeesCoverage(obj) {
+  if (obj === undefined) return filtraPeao;
+  if (obj.id) {
+    buscaPeao = filtraPeao.find((curr) => curr.id === obj.id);
+  }
+  if (obj.name) {
+    buscaPeao = filtraPeao.find((curr) => curr.fullName.includes(obj.name));
+  }
+  if (typeof buscaPeao === 'undefined') throw new Error('Informações inválidas');
+  return buscaPeao;
 }
 
 module.exports = getEmployeesCoverage;
